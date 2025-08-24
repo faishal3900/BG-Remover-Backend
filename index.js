cdd2f4136476113cc8f9af6dd2230768abc2deaf
faishal3900 <faishal3900@gmail.com>
2025-08-24 13:57:40 +0530

    update

--- index.js
+++ index.js
@@ -159,26 +159,9 @@ const allowedOrigins = [
   "http://localhost:5174",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps, curl requests)
-      if (!origin) return callback(null, true);
+app.use(cors());
 
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-  })
-);
-
-app.options("/", cors()); // Valid route
+// app.options("/", cors()); // Valid route
 
 // Handle preflight requests
 // app.options("*", cors());
